1d7ddcc92f4b2da207ab3fb1dbc3bed0b355b1bc
teresaqhoang <tehoang@microsoft.com>
2024-05-07 14:52:05 -0700

    Adding back JSON optimization with catch for empty optimizations

--- webapi/Plugins/Chat/MsGraphOboPlugin.cs
+++ webapi/Plugins/Chat/MsGraphOboPlugin.cs
@@ -123,8 +123,8 @@ public sealed class MsGraphOboPlugin
             }
         }
 
-        // TODO: Dynamically determine token limit
-        return JsonUtils.OptimizeOdataResponseJson(graphResponseContent, this._responseTokenLimit);
+        var optimizedResponse = JsonUtils.OptimizeOdataResponseJson(graphResponseContent, this._responseTokenLimit);
+        return optimizedResponse.Length > 0 ? optimizedResponse : graphResponseContent;
     }
 
     private async Task<string> GetOboAccessTokenAsync(string graphScopes, CancellationToken cancellationToken)
